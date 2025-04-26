f739360ad3ac46d34cc45b0fa844f4fd028afc4b
iremayvaz <iremayvazz28@gmail.com>
2025-04-26 16:06:58 +0300

    homework-01

---
+++ app/src/main/java/com/example/pupilica_bootcamp/degiskenler/Odev_1.kt
@@ -0,0 +1,56 @@
+package com.example.pupilica_bootcamp.degiskenler
+
+fun main(){
+    // İrem AYVAZ
+    // irem.ayvaz@stu.fsm.edu.tr
+
+    // VARIABLES
+    var sehir = "Istanbul"
+    var telefon = 5511107877
+    var posta_kodu = 34762
+    var email = "irem.ayvaz@stu.fsm.edu.tr"
+    var stok_miktari = 10
+    var musteri_adi = "İrem AYVAZ"
+    var bakiye = 1000
+    var maas = 60000
+    var medeni_durum = "Bekar"
+    var urun_yorum = "Defolu urun"
+    var odeme = true
+    var indirim_miktari = 50.49
+    var enlem = 49.5
+    var boylam = 19.2
+    var yemek_fiyati = 500
+    var marka = "LC Waikiki"
+    var muzik_adi = "Canın sağolsun"
+    var urun_puani = 4.8
+    var renk = "black"
+    var ekran_boyutu = 6.6
+    var agirlik = 55
+    var kalan_dakika = 25.5
+    var kupon_suresi = "5 saat"
+
+    // CONSTANTS
+    val ulke = "TURKIYE"
+    val meslek = "Mühendis"
+    val dogum_gunu = "01.10.2003"
+    val odeme_tarihi = "25.04.2025"
+    val siparis_adeti = 19
+    val araba_modeli = "Megane"
+    val kitap_adi = "Dilin Afetleri"
+    val yayinlama_tarihi = "01.01.1900"
+    val oda_sayisi = 4
+    val urun_adi = "Kitap"
+    val video_suresi = 1.53
+    val resim_adi = "Manzara"
+    val dosya_formati = ".jpeg"
+    val renk_kodu = "255"
+    val telefon_modeli = 15
+    val ulusal_gun = "23 nisan"
+    val tatil_gunu = "cumartesi"
+    val rezervasyon_tarihi = "22.06.2025"
+    val sokak_adi = "Haci Rustemoglu"
+    val otobus_hatti = 14
+    val takip_kodu = "TS47835Y3"
+    val kupon_kodu = "CZA40BAHAR"
+    val fatura_adresi = "Umraniye"
+}
