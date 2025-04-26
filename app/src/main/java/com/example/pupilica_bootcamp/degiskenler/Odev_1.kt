2891112f389291e826104da6cf8d1917aece3c97
iremayvaz <iremayvazz28@gmail.com>
2025-04-26 16:24:46 +0300

    homework-01-son

--- app/src/main/java/com/example/pupilica_bootcamp/degiskenler/Odev_1.kt
+++ app/src/main/java/com/example/pupilica_bootcamp/degiskenler/Odev_1.kt
@@ -6,7 +6,7 @@ fun main(){
 
     // VARIABLES
     var sehir = "Istanbul"
-    var telefon = 5511107877
+    var telefon = 5123456789
     var posta_kodu = 34762
     var email = "irem.ayvaz@stu.fsm.edu.tr"
     var stok_miktari = 10
@@ -32,7 +32,7 @@ fun main(){
     // CONSTANTS
     val ulke = "TURKIYE"
     val meslek = "Mühendis"
-    val dogum_gunu = "01.10.2003"
+    val dogum_gunu = "01.10.2000"
     val odeme_tarihi = "25.04.2025"
     val siparis_adeti = 19
     val araba_modeli = "Megane"
@@ -48,9 +48,57 @@ fun main(){
     val ulusal_gun = "23 nisan"
     val tatil_gunu = "cumartesi"
     val rezervasyon_tarihi = "22.06.2025"
-    val sokak_adi = "Haci Rustemoglu"
+    val sokak_adi = "Merkezefendi"
     val otobus_hatti = 14
     val takip_kodu = "TS47835Y3"
     val kupon_kodu = "CZA40BAHAR"
-    val fatura_adresi = "Umraniye"
+    val fatura_adresi = "Fatih"
+
+    // PRINT
+    println("Şehir : $sehir")
+    println("Ülke : $ulke")
+    println("Telefon : $telefon")
+    println("Posta Kodu : $posta_kodu")
+    println("Email : $email")
+    println("Meslek : $meslek")
+    println("Stok Miktarı : $stok_miktari")
+    println("Müşteri Adı : $musteri_adi")
+    println("Bakiye : $bakiye")
+    println("Doğum Günü : $dogum_gunu")
+    println("Maaş : $maas")
+    println("Medeni Durum : $medeni_durum")
+    println("Ürün Yorum : $urun_yorum")
+    println("Ödeme Tarihi : $odeme_tarihi")
+    println("Ödeme : $odeme")
+    println("Sipariş Adeti : $siparis_adeti")
+    println("Araba Modeli : $araba_modeli")
+    println("Kitap Adı : $kitap_adi")
+    println("Yayınlama Tarihi : $yayinlama_tarihi")
+    println("İndirim Miktarı : $indirim_miktari")
+    println("Oda Sayısı : $oda_sayisi")
+    println("Enlem : $enlem")
+    println("Boylam : $boylam")
+    println("Ürün Adı : $urun_adi")
+    println("Yemek Fiyatı : $yemek_fiyati")
+    println("Marka : $marka")
+    println("Müzik Adı : $muzik_adi")
+    println("Video Süresi : $video_suresi")
+    println("Ürün Puanı : $urun_puani")
+    println("Resim Adı : $resim_adi")
+    println("Dosya Formatı : $dosya_formati")
+    println("Renk : $renk")
+    println("Renk Kodu : $renk_kodu")
+    println("Telefon Modeli : $telefon_modeli")
+    println("Ekran Boyutu : $ekran_boyutu")
+    println("Ağırlık : $agirlik")
+    println("Ulusal Gün : $ulusal_gun")
+    println("Tatil Günü : $tatil_gunu")
+    println("Rezervasyon Tarihi : $rezervasyon_tarihi")
+    println("Sokak Adı : $sokak_adi")
+    println("Otobüs Hattı : $otobus_hatti")
+    println("Kalan Dakika : $kalan_dakika")
+    println("Takip Kodu : $takip_kodu")
+    println("Kupon Süresi : $kupon_suresi")
+    println("Kupon Kodu : $kupon_kodu")
+    println("Fatura Adresi : $fatura_adresi")
 }
